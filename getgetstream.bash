74e486d37e905544b41d66b7f827b0651846762f
David Richards <david@radcompany.us>
2023-11-18 06:46:18 -0500

    oops forgot to save and run the file before commiting

--- getgetstream.bash
+++ getgetstream.bash
@@ -2,6 +2,12 @@
 
 while [ 1 ]
 do
+  echo "we beging getgetstream.bash ing here"
   uname -a
-  sleep 2
+  date
+  wget "http://192.168.1.1:8000/stream"
+  echo "hrm waiting 26 seconds"
+  uname -a
+  date
+  sleep 26
 done
