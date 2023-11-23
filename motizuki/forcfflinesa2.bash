6353de7e1c4244259c7a525495d90b1d150d84a2
David Richards <david@radcompany.us>
2023-11-23 07:49:27 -0500

    progress documentation notes lab notes ships log captains dog

---
+++ motizuki/forcfflinesa2.bash
@@ -0,0 +1,274 @@
+makeotf -f font-0355.pfa
+
+makeotf -f font-0394.pfa
+
+makeotf -f font-0396.pfa
+
+makeotf -f font-0402.pfa
+
+makeotf -f font-0403.pfa
+
+makeotf -f font-0404.pfa
+
+makeotf -f font-0407.pfa
+
+makeotf -f font-0411.pfa
+
+makeotf -f font-0414.pfa
+
+makeotf -f font-0417.pfa
+
+makeotf -f font-0421.pfa
+
+makeotf -f font-0424.pfa
+
+makeotf -f font-0427.pfa
+
+makeotf -f font-0430.pfa
+
+makeotf -f font-0433.pfa
+
+makeotf -f font-0436.pfa
+
+makeotf -f font-0439.pfa
+
+makeotf -f font-0442.pfa
+
+makeotf -f font-0445.pfa
+
+makeotf -f font-0447.pfa
+
+makeotf -f font-0450.pfa
+
+makeotf -f font-0454.pfa
+
+makeotf -f font-0516.pfa
+
+makeotf -f font-0519.pfa
+
+makeotf -f font-0522.pfa
+
+makeotf -f font-0525.pfa
+
+makeotf -f font-0528.pfa
+
+makeotf -f font-0531.pfa
+
+makeotf -f font-0534.pfa
+
+makeotf -f font-0537.pfa
+
+makeotf -f font-0540.pfa
+
+makeotf -f font-0543.pfa
+
+makeotf -f font-0546.pfa
+
+makeotf -f font-0549.pfa
+
+makeotf -f font-0552.pfa
+
+makeotf -f font-0740.pfa
+
+makeotf -f font-0742.pfa
+
+makeotf -f font-0745.pfa
+
+makeotf -f font-0748.pfa
+
+makeotf -f font-0751.pfa
+
+makeotf -f font-0754.pfa
+
+makeotf -f font-0757.pfa
+
+makeotf -f font-0760.pfa
+
+makeotf -f font-0763.pfa
+
+makeotf -f font-0766.pfa
+
+makeotf -f font-0769.pfa
+
+makeotf -f font-0772.pfa
+
+makeotf -f font-0776.pfa
+
+makeotf -f font-0779.pfa
+
+makeotf -f font-0782.pfa
+
+makeotf -f font-0785.pfa
+
+makeotf -f font-0788.pfa
+
+makeotf -f font-0791.pfa
+
+makeotf -f font-0792.pfa
+
+makeotf -f font-0793.pfa
+
+makeotf -f font-0796.pfa
+
+makeotf -f font-0799.pfa
+
+makeotf -f font-0802.pfa
+
+makeotf -f font-0805.pfa
+
+makeotf -f font-0808.pfa
+
+makeotf -f font-0810.pfa
+
+makeotf -f font-0813.pfa
+
+makeotf -f font-0816.pfa
+
+makeotf -f font-0819.pfa
+
+makeotf -f font-0822.pfa
+
+makeotf -f font-0825.pfa
+
+makeotf -f font-0828.pfa
+
+makeotf -f font-0831.pfa
+
+makeotf -f font-0834.pfa
+
+makeotf -f font-0837.pfa
+
+makeotf -f font-0840.pfa
+
+makeotf -f font-0841.pfa
+
+makeotf -f font-0843.pfa
+
+makeotf -f font-0845.pfa
+
+makeotf -f font-0846.pfa
+
+makeotf -f font-0848.pfa
+
+makeotf -f font-0849.pfa
+
+makeotf -f font-0850.pfa
+
+makeotf -f font-0853.pfa
+
+makeotf -f font-0856.pfa
+
+makeotf -f font-0859.pfa
+
+makeotf -f font-0862.pfa
+
+makeotf -f font-0865.pfa
+
+makeotf -f font-0868.pfa
+
+makeotf -f font-0871.pfa
+
+makeotf -f font-0874.pfa
+
+makeotf -f font-0877.pfa
+
+makeotf -f font-0880.pfa
+
+makeotf -f font-0883.pfa
+
+makeotf -f font-0885.pfa
+
+makeotf -f font-0886.pfa
+
+makeotf -f font-0888.pfa
+
+makeotf -f font-0889.pfa
+
+makeotf -f font-0891.pfa
+
+makeotf -f font-0892.pfa
+
+makeotf -f font-0894.pfa
+
+makeotf -f font-0895.pfa
+
+makeotf -f font-0897.pfa
+
+makeotf -f font-0898.pfa
+
+makeotf -f font-0900.pfa
+
+makeotf -f font-0901.pfa
+
+makeotf -f font-0903.pfa
+
+makeotf -f font-0904.pfa
+
+makeotf -f font-0907.pfa
+
+makeotf -f font-0910.pfa
+
+makeotf -f font-0913.pfa
+
+makeotf -f font-0916.pfa
+
+makeotf -f font-0919.pfa
+
+makeotf -f font-0922.pfa
+
+makeotf -f font-0932.pfa
+
+makeotf -f font-0935.pfa
+
+makeotf -f font-0938.pfa
+
+makeotf -f font-0941.pfa
+
+makeotf -f font-0944.pfa
+
+makeotf -f font-0947.pfa
+
+makeotf -f font-0950.pfa
+
+makeotf -f font-0953.pfa
+
+makeotf -f font-0956.pfa
+
+makeotf -f font-0959.pfa
+
+makeotf -f font-0962.pfa
+
+makeotf -f font-0965.pfa
+
+makeotf -f font-0968.pfa
+
+makeotf -f font-0971.pfa
+
+makeotf -f font-0980.pfa
+
+makeotf -f font-0983.pfa
+
+makeotf -f font-0986.pfa
+
+makeotf -f font-0989.pfa
+
+makeotf -f font-0992.pfa
+
+makeotf -f font-0995.pfa
+
+makeotf -f font-0998.pfa
+
+makeotf -f font-1001.pfa
+
+makeotf -f font-1004.pfa
+
+makeotf -f font-1007.pfa
+
+makeotf -f font-1010.pfa
+
+makeotf -f font-1013.pfa
+
+makeotf -f font-1016.pfa
+
+makeotf -f font-1019.pfa
+
