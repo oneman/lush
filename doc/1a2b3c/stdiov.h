6c57dc8533f6366cfd88bcca319e69ed96ef70c4
David Richards <david@radcompany.us>
2023-11-27 07:15:34 -0500

    new data

--- doc/1a2b3c/stdiov.h
+++ doc/1a2b3c/stdiov.h
@@ -202,4 +202,109 @@ uint8_t isvsciiword(char *string, size_t len) {
   return len;
 }
 
+
+/*
+ * UTF-8 sequence.
+ * 11000010-11011111 C2-DF start of 2-byte sequence
+ * 11100000-11101111 E0-EF start of 3-byte sequence
+ * 11110000-11110100 F0-F4 start of 4-byte sequence
+ */
+
+#include <sys/types.h>
+
+#include <stdlib.h>
+
+static const wchar_t utf8_modifier_table[] = {
+	0x1F1E6,
+	0x1F1E7,
+	0x1F1E8,
+	0x1F1E9,
+	0x1F1EA,
+	0x1F1EB,
+	0x1F1EC,
+	0x1F1ED,
+	0x1F1EE,
+	0x1F1EF,
+	0x1F1F0,
+	0x1F1F1,
+	0x1F1F2,
+	0x1F1F3,
+	0x1F1F4,
+	0x1F1F5,
+	0x1F1F6,
+	0x1F1F7,
+	0x1F1F8,
+	0x1F1F9,
+	0x1F1FA,
+	0x1F1FB,
+	0x1F1FC,
+	0x1F1FD,
+	0x1F1FE,
+	0x1F1FF,
+	0x1F3FB,
+	0x1F3FC,
+	0x1F3FD,
+	0x1F3FE,
+	0x1F3FF
+};
+
+/* Has this got a zero width joiner at the end? */
+int
+utf8_has_zwj(const struct utf8_data *ud)
+{
+	if (ud->size < 3)
+		return (0);
+	return (memcmp(ud->data + ud->size - 3, "\342\200\215", 3) == 0);
+}
+
+/* Is this a zero width joiner? */
+int
+utf8_is_zwj(const struct utf8_data *ud)
+{
+	if (ud->size != 3)
+		return (0);
+	return (memcmp(ud->data, "\342\200\215", 3) == 0);
+}
+
+/* Is this a variation selector? */
+int
+utf8_is_vs(const struct utf8_data *ud)
+{
+	if (ud->size != 3)
+		return (0);
+	return (memcmp(ud->data, "\357\270\217", 3) == 0);
+}
+
+/* Is this in the modifier table? */
+int
+utf8_is_modifier(const struct utf8_data *ud)
+{
+	wchar_t	wc;
+
+	if (utf8_towc(ud, &wc) != UTF8_DONE)
+		return (0);
+	if (!utf8_in_table(wc, utf8_modifier_table,
+	    nitems(utf8_modifier_table)))
+		return (0);
+	return (1);
+}
+
+
+int utf8(u_char ch)
+{
+	memset(ud, 0, sizeof *ud);
+	if (ch >= 0xc2 && ch <= 0xdf)
+		ud->size = 2;
+	else if (ch >= 0xe0 && ch <= 0xef)
+		ud->size = 3;
+	else if (ch >= 0xf0 && ch <= 0xf4)
+		ud->size = 4;
+	else
+		return (UTF8_ERROR);
+	utf8_append(ud, ch);
+	return (UTF8_MORE);
+}
+
+
+
 #endif
