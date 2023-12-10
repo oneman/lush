01b9fc78512435f9a4405d44079c10565afdc761
David Richards <david@radcompany.us>
2023-12-10 03:20:33 -0500

    neet

--- demo/teardown.c
+++ demo/teardown.c
@@ -1,3 +1,15 @@
 void teardown(uint8_t *bs, size_t sz) {
+  int i;
+  int bitcnt[256];
   printf("! %p %lu bytes\n", bs, sz);
+
+  for (i = 0; i < 256; i++) {
+    bitcnt[i] = 0;
+  }
+  for (i = 0; i < sz; i++) {
+    bitcnt[bs[i]]++;
+  }
+  for (i = 0; i < 256; i++) {
+    printf("%d: %d\n", i, bitcnt[i]);
+  }
 }
