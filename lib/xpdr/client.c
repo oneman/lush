8cecbc5f2ac44915c3c60c0e2d1deca044dbe341
David Richards <david@radcompany.us>
2024-02-27 04:39:14 -0500

    small fix

--- lib/xpdr/client.c
+++ lib/xpdr/client.c
@@ -8,20 +8,13 @@ int kr_xpdr_make(kr_client *client, char *name, kr_xpdr_path_info *info) {
   if (client == NULL) return -1;
   if (name == NULL) return -2;
   if (info == NULL) return -3;
-  //if (kr_xpdr_path_info_valid(info) < 0) {
-  // return -1;
-  //}
-  char string[8192];
-  kr_xpdr_path_info_to_text(string, info, sizeof(string));
-  printk("XPDR: mkpath-\n%s", string);
-
-  snprintf(crate.address, sizeof(crate.address), "/xpdr/%s", name);
+  crate.addr_len = snprintf(crate.address, sizeof(crate.address), "/xpdr/%s", name);
   crate.method = KR_PUT;
   crate.type = KR_XPDR_PATH_INFO;
   crate.info.xpdr_path_info = *info;
   char json[4096];
   ret = kr_crate_pack_json(json, &crate, sizeof(json));
-  printk("%s",json);
+  printk("%s", json);
   ret = kr_crate_send(client, &crate);
   return ret;
 }
