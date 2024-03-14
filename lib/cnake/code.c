92191b7c698c1a8985cff66177eb1e86e725d72e
David Richards <david@radcompany.us>
2024-03-14 10:01:04 -0400

    pointy

--- lib/cnake/code.c
+++ lib/cnake/code.c
@@ -20,9 +20,12 @@ typedef struct {
   int one;
 } datatype;
 
+#include "key.c"
 #include "code2.c"
 
 void cnake(void) {
+  key();
+  exit(0);
   int s = 25 * 60;
   int a = s / 262;
   printk("Game: %d !", a);
