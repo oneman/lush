72c93599e01fd3e54e538bbcc1237bdd0918ce63
David Richards <david@radcompany.us>
2024-03-29 06:41:59 -0400

    ya

--- lib/cnake/text.c
+++ lib/cnake/text.c
@@ -1,6 +1,80 @@
 #ifndef C_TEXT
 #define C_TEXT
 
+#define UNICODES 1112064
+#define PHY_C 299792458
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+typedef unsigned char u8;
+typedef unsigned short int u16;
+typedef unsigned long int u64;
+#define fubar exit(1);
+
+typedef struct {
+  size_t sz;
+  u64 spaces;
+  u64 letters;
+  u64 numbers;
+  u64 dodads;
+  u64 uppercase_letters;
+  u64 vowels;
+  u64 consonants;
+  u64 sylables;
+  u64 words;
+  u64 n[128];
+  //int apostrophe_s;
+} text_nfo;
+
+void anal_text(u8 *buf, size_t sz) {
+  if (!buf) fubar;
+  if (sz < 1) fubar;
+  /* Assumptions: We have one line of valid ascii text */
+  text_nfo nfo;
+  nfo.sz = sz;
+  int n = 0;
+  for (n = 0; n < sz; n++) {
+    char c = buf[n];
+    if (c == ' ') {
+      nfo.spaces++;
+      continue;
+    }
+    if (isalpha(c)) {
+      nfo.letters++;
+    } else if (isdigit(c)) {
+      nfo.numbers++;
+    }
+  }
+  printf("Length: %lu\n", nfo.sz);
+  printf("Numbers: %lu\n", nfo.numbers);
+  printf("Letters: %lu\n", nfo.letters);
+  printf("Spaces: %lu\n", nfo.spaces);
+  printf("Length: %lu characters\n", nfo.sz);
+}
+
+void anal_test() {
+  char *customer_statement = "\
+Let's do that thing with, as an inline example, this silly sentence, the one\
+your reading right here starting with Let's and ending soon after here with\
+some immediate regrets.";
+  anal_text(customer_statement, strlen(customer_statement));
+  return 26;
+}
+
+/*
+13312 - 19893 CJK Ideographs Extension A
+19968 - 40869 CJK Ideographs
+44032 - 55203 Hangul Syllables
+55296 - 56191 Non-Private Use High Surrogates
+56192 - 56319 Private Use High Surrogates
+56320 - 57343 Low Surrogates
+57344 - 63743 The Private Use Area
+983040 - 1048573 Private Use
+1048576 - 1114109 Private Use*/
+
 u8 is_ascii_blank(u8 byte) {
   if (byte == SP) return 1;
   if (byte == LF) return 1;
